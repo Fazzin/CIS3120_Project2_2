603971e064e66f42d98d5c0a76e3aee98437136e
Fazzin <71731841+Fazzin@users.noreply.github.com>
2025-05-05 14:53:12 -0400

    Added introduction function

---
+++ src/introduction.py
@@ -0,0 +1,10 @@
+def introduction():
+    print('''
+          The project aims to determine the average smaller is taller than the average volleyball player.
+          It does this by scraping the data of athletes names and heights from two different sports, volleyball, and swimming & diving.
+          It pulls from 9 different universities for volleyball and 10 different universities for swimming & diving. 
+          It also finds the names and heights of the athelets with the 5 tallest and shortest heights for both men's and women's teams.
+          The program uses the data to create a dataframe and a bar graph showing the average height distribution across the 4 different
+          team categories (men's swimming, men's volleyball, women's swimming, women's volleyball). Finally it exports the data as a csv.
+          ''')
+    
