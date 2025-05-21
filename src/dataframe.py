ed0080beea2646689c3d8f656aa010b7f9dda905
linhlinh28-gif <linhle281203@gmail.com>
2025-05-21 19:46:14 -0400

    Update dataframe.py

--- src/dataframe.py
+++ src/dataframe.py
@@ -1,3 +1,14 @@
 '''
 Compiles a dataframe of all the names and heights of the players on the men and women's sports team.
 '''
+
+import pandas as pd
+
+def sports_dataframe(names, heights):
+    data = {
+        'Name' : names,
+        'Height' : heights
+    }
+    df = pd.DataFrame(data)
+    avg_height = sum(heights) / len(heights) #Divides the sum of the heights and the total heights to find the average
+    return df, avg_height
